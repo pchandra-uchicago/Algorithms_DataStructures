5d4412f0f6d7ab16bb702c490b2aa03e0eaee1bd
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-09-22 12:29:26 -0500

    Update MatrixSpiral.cpp

--- MatrixSpiral.cpp
+++ MatrixSpiral.cpp
@@ -9,7 +9,6 @@ using namespace std;
 vector<int> printInSpiralOrder(const vector<vector<int> > &A)
 {
 	vector<int> result;
-
 	int dir = 0;
 	int top = 0; int bottom = A.size() - 1; int left = 0; int right = A[0].size() - 1;
 
@@ -47,7 +46,6 @@ vector<int> printInSpiralOrder(const vector<vector<int> > &A)
 			}			
 			left++;
 		}
-
 		dir = (dir + 1) % 4;
 	}
 	return result;
@@ -56,7 +54,6 @@ vector<int> printInSpiralOrder(const vector<vector<int> > &A)
 int main()
 {
 	vector< vector<int> > vec;
-
 	for (int i = 1; i < 4; i++) {
 		vector<int> row; // Create an empty row
 		for (int j = 0; j < 4; j++) {
@@ -64,14 +61,11 @@ int main()
 		}
 		vec.push_back(row); // Add the row to the main vector
 	}
-
 	vector<int> result = printInSpiralOrder(vec);
-	
 	for (vector<int>::iterator i = result.begin(); i != result.end(); i++)
 	{
 		cout << *i << ',';
 	}
-
 	_getch();
 	return 0;
 }

1c59946bad7bbfcf42ea9e6246e432ca7ff91143
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-09-24 13:56:07 -0500

    Update SpiralOrderMatrixII.cpp

--- SpiralOrderMatrixII.cpp
+++ SpiralOrderMatrixII.cpp
@@ -6,49 +6,39 @@
 
 using namespace std;
 
-vector<vector<int> > generateMatrix(int A)
-{
+vector<vector<int> > generateMatrix(int A) {
 	vector<vector<int>> result(A);
 	int dir = 0; int num = 1;
 	int top = 0; int bottom = A- 1; int left = 0; int right = A - 1;
+	
 	/*creates a dummy 2d vector in matrix form*/
-	for (int i = top; i <= bottom; i++)
-	{
+	for (int i = top; i <= bottom; i++) {
 		result[i].resize(A);
 	}
-	while (top <= bottom && left <= right)
-	{
-		if (dir == 0)
-		{
-			for (int i = left; i <= right; i++)
-			{
+	while (top <= bottom && left <= right) {
+		if (dir == 0) {
+			for (int i = left; i <= right; i++) {
 				result[top][i] = num;
 				num++;
 			}
 			top++;
 		}
-		else if (dir == 1)
-		{
-			for (int i = top; i <= bottom; i++)
-			{
+		else if (dir == 1) {
+			for (int i = top; i <= bottom; i++) {
 				result[i][right] = num;
 				num++;				
 			}
 			right--;
 		}
-		else if (dir == 2)
-		{
-			for (int i = right; i >= left; i--)
-			{
+		else if (dir == 2) {
+			for (int i = right; i >= left; i--) {
 				result[bottom][i] = num;
 				num++;				
 			}
 			bottom--;
 		}
-		else if (dir == 3)
-		{
-			for (int i = bottom; i >= top; i--)
-			{
+		else if (dir == 3) {
+			for (int i = bottom; i >= top; i--) {
 				result[i][left] = num;
 				num++;				
 			}

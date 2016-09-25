bdc3506e8d6baa7297f50b40690004d74d3bcfbb
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-09-25 00:38:45 -0500

    Add files via upload

---
+++ AntiDiagonals.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<vector<int> > diagonal(vector<vector<int> > &A) {
+	int row = 0, col = 0;
+	vector<vector<int> > result; vector<int>diag;
+	
+	for (int i = 0; i <= A.size() - 1; i++)
+	{
+		col = i;
+		row = 0;
+		while (col >= 0)
+		{
+			diag.push_back(A[row][col]);
+			row++;
+			col--;
+		}
+		result.push_back(diag);
+		diag.clear();
+	}
+	
+	row = 1; int count = 1;
+	while(row <= A.size() - 1)
+	{
+		col = A.size() - 1;
+		while (row <= A.size() - 1)
+		{
+			diag.push_back(A[row][col]);
+			row++;
+			col--;
+		}
+		result.push_back(diag);
+		count++;
+		row = count;
+		diag.clear();
+	}
+	return result;
+}
+
+int main()
+{
+	vector< vector<int> > vec;
+
+	for (int i = 1; i < 4; i++) {
+		vector<int> row; // Create an empty row
+		for (int j = 1; j < 4; j++) {
+			row.push_back(i * j); // Add an element (column) to the row
+		}
+		vec.push_back(row); // Add the row to the main vector
+	}
+
+	diagonal(vec);
+	_getch();
+	return 0;
+}

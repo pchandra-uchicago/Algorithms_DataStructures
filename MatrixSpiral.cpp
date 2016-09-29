f1e08abab6c75f1346442cf81a33d741b6070b59
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-09-29 01:47:29 -0500

    Update MatrixSpiral.cpp

--- MatrixSpiral.cpp
+++ MatrixSpiral.cpp
@@ -6,40 +6,32 @@
 
 using namespace std;
 
-vector<int> printInSpiralOrder(const vector<vector<int> > &A)
-{
+vector<int> printInSpiralOrder(const vector<vector<int> > &A) {
 	vector<int> result;
 	int dir = 0;
 	int top = 0; int bottom = A.size() - 1; int left = 0; int right = A[0].size() - 1;
 
-	while (top <= bottom && left <= right)
-	{
-		if(dir == 0)
-		{
-			for (int i = left; i <= right ; i++)
-			{
+	while (top <= bottom && left <= right) {
+		if(dir == 0) {
+			for (int i = left; i <= right ; i++) {
 				result.push_back(A[top][i]);
 			}
 			top++;			
 		}
-		else if (dir == 1)
-		{
-			for (int i = top; i <= bottom; i++)
-			{
+		else if (dir == 1) {
+			for (int i = top; i <= bottom; i++) {
 				result.push_back(A[i][right]);
 			}
 			right--;			
 		}
-		else if (dir == 2)
-		{
+		else if (dir == 2) {
 			for (int i = right; i >= left; i--)
 			{
 				result.push_back(A[bottom][i]);
 			}
 			bottom--;			
 		}
-		else if (dir==3)
-		{
+		else if (dir==3) {
 			for (int i = bottom; i >= top; i--)
 			{
 				result.push_back(A[i][left]);

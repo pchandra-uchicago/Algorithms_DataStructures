2d1c291cc1ec1c0de879e77d61ca69cbd7f0bd19
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-10-02 18:16:34 -0500

    Add files via upload

---
+++ OddOccurencesOfNumber.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int getOddOccurences(vector<int> A) {
+	int result = 0;
+	for (int i = 0; i < (A.size()/A[0])-1; i++) {
+		result = result ^ A[i];
+	}
+	return result;
+}
+
+int main()
+{
+	vector<int> A;
+	A.push_back(2);
+	A.push_back(3);
+	A.push_back(5);
+	A.push_back(4);
+	A.push_back(5);
+	A.push_back(2);
+	A.push_back(4);
+	A.push_back(3);
+	A.push_back(5);
+	A.push_back(2);
+	A.push_back(4);
+	A.push_back(4);
+	A.push_back(2);
+
+	cout << "The odd occurences are " << getOddOccurences(A);
+	_getch();
+	return 0;
+}

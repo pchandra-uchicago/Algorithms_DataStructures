81d5d062a0991cfb79b188e6b093c55de8a3a616
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-10-13 20:20:02 -0500

    Add files via upload

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -10,18 +10,6 @@ struct  Node
 	Node* next;
 };
 
-int length(Node* head)
-{
-	int len = 0;
-	while (head != NULL)
-	{
-		head = head->next;
-		len++;
-	}
-	return len;
-}
-
-
 void Insert(Node** head, int data, int n)
 {
 	Node* current = new Node();
@@ -44,72 +32,60 @@ void Insert(Node** head, int data, int n)
 
 Node* mergeTwoLists(Node* A, Node* B)
 {
-	Node* listA = A; Node* listB = B;
-	Node* sortedList = new Node();
-	int sizeA, sizeB;
+	if (A == NULL) return B;
+	if (B == NULL) return A;
+
+	Node* listA = A; Node* listB = B; Node* sortedHead = NULL;	
+	if (listA->data < listB->data)
+	{
+		sortedHead = listA;
+		listA = listA->next;
+	}
+	else 
+	{
+		sortedHead = listB;
+		listB = listB->next;
+	}
+	Node* sortedList = sortedHead;
 	while (listA != NULL && listB != NULL)
 	{
 		if (listA->data < listB->data)
 		{
-			sortedList->data = listA->data;
-			sortedList = sortedList->next;
-			listA = listA->next;
-			sizeA++;
+			sortedList->next = listA;							
+			listA = listA->next;			
 		}
-		else if (listA->data > listB->data)
+		else 
 		{
-			sortedList->data = listB->data;
-			sortedList = sortedList->next;
-			listB = listB->next;
-			sizeB++;
-		}
-		else
-		{
-			sortedList->data = listA->data;
-			sortedList = sortedList->next;
-			listA = listA->next;
-			listB = listB->next;
-			sizeA++; sizeB++;
-		}
-	}
-
-	/*add the remaining items fro either list*/
-	if (sizeA != length(A))
-	{
-		for (int i = 0; i < length(A) - sizeA; i++)
-		{
-			sortedList->data = listA->data;
-			sortedList = sortedList->next;
-			listA = listA->next;
-		}
-	}
-	else if (sizeB != length(B))
-	{
-		for (int i = 0; i < length(A) - sizeA; i++)
-		{
-			sortedList->data = listB->data;
-			sortedList = sortedList->next;
+			sortedList->next = listB;							
 			listB = listB->next;			
 		}
+		sortedList = sortedList->next;
+	}	
+	/*add the leftover elements*/
+	if (listA)
+	{
+		sortedList->next = listA;
 	}
-	return sortedList;
+	else
+	{
+		sortedList->next = listB;
+	}
+
+	return sortedHead;
 }
 
 int main()
 {
 	Node* head1 = NULL;
 	Insert(&head1, 2, 1);
-	Insert(&head1, 4, 2);
-	Insert(&head1, 9, 3);
-	Insert(&head1, 12, 4);
+	Insert(&head1, 4, 2);	
 
 	Node* head2 = NULL;
 	Insert(&head2, 3, 1);
 	Insert(&head2, 6, 2);
-	Insert(&head2, 7, 3);
-	Insert(&head2, 11, 4);
-
+	
 	Node *result = mergeTwoLists(head1, head2);
 	_getch();
 	return 0;
 }
+

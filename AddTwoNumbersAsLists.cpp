fc68808fd3ec878010f4896b7627fbde756ddd27
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-10-14 19:42:06 -0500

    Update AddTwoNumbersAsLists.cpp

--- AddTwoNumbersAsLists.cpp
+++ AddTwoNumbersAsLists.cpp
@@ -7,67 +7,55 @@
 #include<numeric>
 
 using namespace std;
-
-struct  Node
-{
+struct  Node {
 	int data;
 	Node* next;
 };
 
-void Insert(Node** head, int data, int n)
-{
+void Insert(Node** head, int data, int n) {
 	Node* current = new Node();
 	current->data = data;
 	current->next = NULL;
-	if (n == 1)
-	{
+	if (n == 1) {
 		current->next = *head;
 		*head = current;
 		return;
 	}
 	Node* prev = *head;
-	for (int i = 0; i < n - 2; i++)
-	{
+	for (int i = 0; i < n - 2; i++) {
 		prev = prev->next;
 	}
 	current->next = prev->next;
 	prev->next = current;
 }
 
-Node* addTwoNumbers(Node* A, Node* B)
-{
+Node* addTwoNumbers(Node* A, Node* B) {
 	Node* listA = A; Node* listB = B;
 	vector<string> strA; vector<string> strB;
-	while (listA || listB)
-	{
-		if (listA)
-		{
+	while (listA || listB) {
+		if (listA) {
 			strA.push_back(to_string(listA->data));
 			listA = listA->next;
 		}
-		if (listB)
-		{
+		if (listB) {
 			strB.push_back(to_string(listB->data));
 			listB = listB->next;
 		}		
 	}
 	std::reverse(strA.begin(), strA.end());
 	std::reverse(strB.begin(), strB.end());
-
 	string sum = to_string(stoi(accumulate(strA.begin(), strA.end(), string(""))) + 
 							stoi(accumulate(strB.begin(), strB.end(), string(""))));
 	reverse(sum.begin(), sum.end());
 
 	Node* head = NULL;
-	for (int i = 0; i < sum.size(); i++)
-	{
+	for (int i = 0; i < sum.size(); i++) {
 		Node* current = new Node();
 		current->data = (int)(sum.at(i)) - 48;
 		current->next = NULL;
 		if (i == 0)
 			head = current;
-		else
-		{
+		else {
 			Node* prev = head;
 			while (prev->next != NULL)
 			{

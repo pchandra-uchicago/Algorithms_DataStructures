b3088ca333a2965fe8bb0e3d5b274ebc59dfa397
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-10-14 22:09:03 -0500

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -3,39 +3,33 @@
 #include<stdio.h>
 
 using namespace std;
-
-struct  Node
-{
+struct  Node {
 	int data;
 	Node* next;
 };
 
-void Insert(Node** head, int data, int n)
-{
+void Insert(Node** head, int data, int n) {
 	Node* current = new Node();
 	current->data = data;
 	current->next = NULL;
-	if (n == 1)
-	{
+	if (n == 1) {
 		current->next = *head;
 		*head = current;
 		return;
 	}
 	Node* prev = *head;
-	for (int i = 0; i < n - 2; i++)
-	{
+	for (int i = 0; i < n - 2; i++)	{
 		prev = prev->next;
 	}
 	current->next = prev->next;
 	prev->next = current;
 }
 
-Node* reverseLinkedList(Node* A)
-{
+/* iterative method*/
+Node* reverseLinkedList(Node* A) {
 	Node* current; Node* prev = NULL; Node* next; Node* head = A;
 	current = head;
-	while (current != NULL)
-	{
+	while (current != NULL)	{
 		next = current->next;
 		current->next = prev;
 		prev = current;

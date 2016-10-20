695d6c61222340b643a4465b1e3aebb0942fff02
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-10-20 00:47:47 -0500

    Update SortArrayInWaveForm.cpp

--- SortArrayInWaveForm.cpp
+++ SortArrayInWaveForm.cpp
@@ -13,8 +13,7 @@ void sortInWave(int arr[], int n) {
 	// Traverse all even elements
 	for (int i = 0; i < n; i += 2) {		
 		if (i>0 && arr[i - 1] > arr[i])
-			swap(&arr[i], &arr[i - 1]);
-		
+			swap(&arr[i], &arr[i - 1]);		
 		if (i<n - 1 && arr[i] < arr[i + 1])
 			swap(&arr[i], &arr[i + 1]);
 	}

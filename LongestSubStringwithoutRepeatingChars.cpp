7e0d8e046a9186b9f3685f492d62c2af5153d850
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-10-23 12:40:19 -0500

    Add files via upload

---
+++ LongestSubStringwithoutRepeatingChars.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#include<unordered_map>
+#include<iterator>
+
+using namespace std;
+
+int lengthOfLongestSubstring(string A)
+{
+	if (A.size() <= 1)
+		return A.size();
+	
+	char curChar, nextChar; vector<int> strLength; int currIndex = 0; bool flag = true;	
+	for (int i = 1; i < A.size(); i++) {
+		nextChar = A[i];
+		for (int j = currIndex; j < i; j++) {
+			curChar = A[j];
+			if (curChar == nextChar) {				
+				if (i != A.size() - 1) {
+					strLength.push_back(i - currIndex);
+					if (i == j + 1)
+						currIndex = i;
+					else
+						currIndex = j + 1;
+				}
+				else {
+					flag = false;
+				}									
+				break;
+			}			
+		}
+		if (i == A.size()-1) {
+			if (flag)			
+				strLength.push_back(i - currIndex + 1);			
+			else			
+				strLength.push_back(i - currIndex);			
+		}		
+	}
+	return *(max_element(strLength.begin(), strLength.end()));
+}
+
+int main()
+{
+	string str = "dadbc";
+	int temp = str.length();
+	int len = lengthOfLongestSubstring(str);
+	_getch();
+	return 0;	
+}

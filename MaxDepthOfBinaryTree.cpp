b2bbd328b13200c06dd665c8ca943a99c1207e68
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-11-11 11:42:33 -0600

    Add files via upload

---
+++ MaxDepthOfBinaryTree.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct Node {
+	char data;
+	Node* left;
+	Node* right;
+};
+
+Node* getNewNode(int data) {
+	Node* root = new Node();
+	root->left = NULL;
+	root->right = NULL;
+	root->data = data;
+	return root;
+}
+
+Node* Insert(Node* root, int data)
+{
+	if (root == NULL) {
+		root = getNewNode(data);
+		return root;
+	}
+	else if (data <= root->data)
+		root->left = Insert(root->left, data);
+	else
+		root->right = Insert(root->right, data);
+	return root;
+}
+
+
+int maxDepth(Node* root) {
+	if (root == NULL) return 0;
+	return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+int main()
+{
+	Node* root = NULL;
+	root = Insert(root, 15);
+	root = Insert(root, 10);
+	root = Insert(root, 20);
+	root = Insert(root, 30);
+	_getch();
+	return 0;
+}

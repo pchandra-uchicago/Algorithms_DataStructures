330853d7175a57c4fe72cfb3a8c18b0f300b50b6
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-11-16 16:35:39 -0600

    Update QueueImplementationUsingArrays.cpp

--- QueueImplementationUsingArrays.cpp
+++ QueueImplementationUsingArrays.cpp
@@ -7,30 +7,25 @@ using namespace std;
 const int N = 100;
 int front = -1, rear = -1;
 
-bool isEmpty()
-{
+bool isEmpty() {
 	if (front == -1 && rear == -1) return true;
 	else return false;
 }
 
-void enQueue(int x, int *A)
-{
-	if ((rear+1)%N == front)	return;
-	else if (isEmpty())
-		front = rear = 0;
+void enQueue(int x, int *A) {
+	if ((rear+1)%N == front) return;
+	else if (isEmpty()) front = rear = 0;
 	else  rear = (rear + 1)%N;
 	A[rear] = x;
 }
 
-void deQueue()
-{
+void deQueue() {
 	if (isEmpty()) return;
 	else if (front == rear)	front = rear = -1;
 	else front = (front + 1)%N;	
 }
 
-void Print(int *A)
-{
+void Print(int *A) {
 	for (int i = front; i <= rear; i++) {
 		cout << A[i] << "\n";
 	}

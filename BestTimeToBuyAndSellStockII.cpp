677709f56c464e8bc56e7c0a69959b4beaaafef2
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-11-17 12:03:46 -0600

    Update BestTimeToBuyAndSellStockII.cpp

--- BestTimeToBuyAndSellStockII.cpp
+++ BestTimeToBuyAndSellStockII.cpp
@@ -6,20 +6,12 @@
 
 using namespace std;
 
-int maxProfit(const vector<int> &A)
-{
+int maxProfit(const vector<int> &A) {
 	int max_diff = A[1] - A[0];
 	int min_element = A[0];
-	for (int i = 1; i <= A.size() - 1; i++)
-	{
-		if (A[i] - min_element > max_diff)
-		{
-			max_diff = A[i] - min_element;
-		}
-		if (A[i] < min_element)
-		{
-			min_element = A[i];
-		}
+	for (int i = 1; i <= A.size() - 1; i++)	{
+		if (A[i] - min_element > max_diff) max_diff = A[i] - min_element;
+		if (A[i] < min_element)	min_element = A[i];
 	}
 	return max_diff;
 }

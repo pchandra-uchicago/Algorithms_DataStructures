58fa36830a51cd1c18fcd763777c9ec981c95ce8
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-11-18 11:18:41 -0600

    Update ModeInAnArray.cpp

--- ModeInAnArray.cpp
+++ ModeInAnArray.cpp
@@ -6,24 +6,21 @@
 
 using namespace std;
 
-int maxRepeatingElement(int *arr, int n, int k)
-{
+int maxRepeatingElement(int *arr, int n, int k) {
 	for (int i = 0; i< n; i++)
 		arr[arr[i] % k] += k;
 
 	// finds index of most repeating element
 	int max = arr[0], result = 0;
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] > max)
-		{
+	for (int i = 1; i < n; i++) {
+		if (arr[i] > max) {
 			max = arr[i];
 			result = i;
 		}
 	}
-	/* this code gives the original array back
+	/* this code gives the original array back*/
 	for (int i = 0; i< n; i++)
-	arr[i] = arr[i]%k; */
+	arr[i] = arr[i]%k; 
 	return result;
 }
 

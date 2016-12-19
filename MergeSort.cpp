2219558bccc534e49c6204722f8a9290abdc39d8
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-19 01:26:36 -0600

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,37 +5,30 @@
 
 using namespace std;
 
-void merge(int *left, int *right, int *A)
-{
+void merge(int *left, int *right, int *A) {
 	int leftSize = sizeof(left) / sizeof(left[0]);
 	int rightSize = sizeof(right) / sizeof(right[0]);
 	int i =0, j =0, k = 0;
-	while (i < leftSize && j < rightSize)
-	{
-		if (left[i] < right[j])
-		{
+	while (i < leftSize && j < rightSize) {
+		if (left[i] < right[j]) {
 			A[k] = left[i];
 			i = i + 1;
 		}
-		else
-		{
+		else {
 			A[k] = right[j];
 			j = j + 1;
 		}
 		k = k + 1;
 	}
-	while (i < leftSize)
-	{
+	while (i < leftSize) {
 		A[k] = left[i]; i++; k++;
 	}
-	while (j < rightSize)
-	{
+	while (j < rightSize) {
 		A[k] = right[j]; j++; k++;
 	}
 }
 
-void mergeSort(int *A, int size)
-{
+void mergeSort(int *A, int size) {
 	//int size = sizeof(A) / sizeof(A[0]);
 	if (size < 2) return;
 	int mid = size / 2;

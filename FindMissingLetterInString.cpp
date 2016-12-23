1e575aa35c331bfc89f29f30b4896ddd6c6541d8
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-22 20:25:36 -0600

    Add files via upload

---
+++ FindMissingLetterInString.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+char findTheDifference(string s, string t)
+{
+	int i = 0; int sum1 = 0, sum2 = 0;
+	while (i != s.size()) {
+		sum1 += s[i];
+		sum2 += t[i];
+		i++;
+	}
+	sum2 += t[i];
+	return static_cast<char>(sum2-sum1);
+}
+
+int main()
+{
+	cout << "The difference is " << findTheDifference("abcd", "abkcd");
+	_getch();
+	return 0;
+}

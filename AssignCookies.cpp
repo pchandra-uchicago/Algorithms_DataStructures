894c7f7fa17f9a0a58a3f5f976d5235298d46f3d
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-23 02:13:13 -0600

    Update AssignCookies.cpp

--- AssignCookies.cpp
+++ AssignCookies.cpp
@@ -1,3 +1,10 @@
+/*Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie. 
+Each child i has a greed factor gi, which is the minimum size of a cookie that the child will be content with; and each cookie j has 
+a size sj. If sj >= gi, we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number 
+of your content children and output the maximum number.
+Note:
+You may assume the greed factor is always positive. 
+You cannot assign more than one cookie to one child.*/
 #include<iostream>
 #include<conio.h>
 #include<stdio.h>
@@ -6,12 +13,11 @@
 
 using namespace std;
 
-int findContentChildren(vector<int>& g, vector<int>& s)
-{
+/*Greedy algorithm*/
+int findContentChildren(vector<int>& g, vector<int>& s) {
 	sort(g.begin(), g.end());
 	sort(s.begin(), s.end()); int i = 0;
-	for (int j = 0; i < g.size() && j < s.size(); j++)
-	{
+	for (int j = 0; i < g.size() && j < s.size(); j++) {
 		if (g[j] < s[j]) i++;
 	}
 	return i;

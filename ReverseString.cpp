7b22470ca3a7f018f81a5104aa071b104e8d1faf
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-22 18:38:59 -0600

    Add files via upload

---
+++ ReverseString.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<string>
+
+using namespace std;
+
+string reverseString(string s) {
+	int i = 0; int j = s.size() - 1;
+	while(i < j) {
+		swap(s[i], s[j]);
+		i++; j--;
+	}
+	return s;
+}
+
+int main()
+{
+	string inputStr = "Whatsupp";
+	reverseString(inputStr);
+	_getch();
+	return 0;
+}

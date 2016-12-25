1468a458dc5940c76a5014eb89978e8b094611f9
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-25 00:52:48 -0600

    Add files via upload

---
+++ MoveZeroesToEnd.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+	int count = 0; int size = nums.size();
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (nums[i] != 0)
+		{
+			nums[count] = nums[i];
+			count++;
+		}
+	}
+	while(count < size)
+		nums[count++] = 0;
+}
+
+int main()
+{
+	vector<int> nums;
+	nums.insert(nums.end(), { 0, 1, 0, 3, 12 });
+	moveZeroes(nums);
+	_getch();
+	return 0;
+}

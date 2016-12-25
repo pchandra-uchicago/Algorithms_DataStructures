efc440ca99ca72e0bfd9e96fb4d8559a9044bc46
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-25 00:23:21 -0600

    Update RepeatedSubStringPattern.cpp

--- RepeatedSubStringPattern.cpp
+++ RepeatedSubStringPattern.cpp
@@ -1,3 +1,6 @@
+/*Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring 
+together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+Input: "abcabcabcabc"  Output: True  Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)*/
 #include<iostream>
 #include<conio.h>
 #include<stdio.h>

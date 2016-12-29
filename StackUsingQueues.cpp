9ce1343ac6147985b4cd020b3e63189b8d1fe48e
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-29 00:21:10 -0500

    Update StackUsingQueues.cpp

--- StackUsingQueues.cpp
+++ StackUsingQueues.cpp
@@ -6,36 +6,65 @@
 
 using namespace std;
 
-void push(int x, queue<int> &q1) {
-	q1.push(x);
-}
+class Stack {
+public:
+    queue<int> q1;
+    // Push element x onto stack.
+    void push(int x) {
+        q1.push(x);
+    }
 
-int pop(queue<int> &q1) {
-	queue<int> q2;
-	int size = q1.size(); int count = 0;
-	if (q1.empty()) return 0;
-	while (count != size - 1) {
-		q2.push(q1.front());
+   // Removes the element on top of the stack.
+    void pop() {
+        if(q1.empty()) return;
+        if (q1.size() == 1) {
+			q1.pop(); 
+			return;
+	}
+        queue<int> q2; int count = 0;
+        while(count < q1.size()) {
+            q2.push(q1.front());
+            q1.pop();
+            count++;
+        }
+        q1.pop();
+        q1 = q2;
+    }
+
+    // Get the top element.
+    int top() {
+        if(q1.empty()) return 0;
+        if (q1.size() == 1) return q1.front();
+        queue<int> q2; int count = 0;
+        while(count < q1.size()) {
+            q2.push(q1.front());
+            q1.pop();
+            count++;
+        }
+        int top = q1.front();
+        q2.push(top);
 		q1.pop();
-		count++;
-	}
-	int popped = q1.front();
-	q1.pop();
-	while (!q2.empty()) {
-		q1.push(q2.front());
-		q2.pop();
-	}
-	return popped;
-}
+		q1 = q2;
+        return top;
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        if(q1.empty())
+            return true;
+        else
+            return false;
+    }
+};
 
 int main()
 {
-	queue<int> q1;
-	push(2, q1);
-	push(4, q1);
-	push(6, q1);
-	push(8, q1);
-	cout<<"Popped element : "<<pop(q1);
+	Stack s;
+	s.push(1);
+	s.push(2);
+	s.pop();
+	s.push(3);
+	cout << " Top element is : " << s.top();
 	_getch();
 	return 0;
 }

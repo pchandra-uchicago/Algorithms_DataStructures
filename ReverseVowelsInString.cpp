c585799102e0e0a4bbc940c79cf097c951b6b9b6
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-29 20:09:03 -0500

    Update ReverseVowelsInString.cpp

--- ReverseVowelsInString.cpp
+++ ReverseVowelsInString.cpp
@@ -1,3 +1,8 @@
+/*Write a function that takes a string as input and reverse only the vowels of a string.
+Example 1:
+Given s = "hello", return "holle".
+Example 2:
+Given s = "leetcode", return "leotcede".*/
 #include<iostream>
 #include<conio.h>
 #include<stdio.h>

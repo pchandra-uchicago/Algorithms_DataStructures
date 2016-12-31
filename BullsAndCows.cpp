e920a79289e16151f8dcbb59c88ad3c22ee379e9
pchandra-uchicago <bcprakashchandra@gmail.com>
2016-12-31 18:12:13 -0500

    Update BullsAndCows.cpp

--- BullsAndCows.cpp
+++ BullsAndCows.cpp
@@ -1,3 +1,8 @@
+/* You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what 
+the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret 
+number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position
+(called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+For example : Secret number:  "1807" Friend's guess: "7810"  Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)*/
 #include<iostream>
 #include<conio.h>
 #include<stdio.h>
